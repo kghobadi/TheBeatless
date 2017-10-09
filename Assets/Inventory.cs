08b8068d6c93abdfd2f71f3d990bf87f62b936d2
Unknown <ksg304@nyu.edu>
2017-10-08 20:27:21 -0400

    Fixed the planting system
    Plant the seed, watch the tree grow, watch food spawn, feed the animals with the food, they poop out seeds, and the cycle continues!

---
+++ Assets/Inventory.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+    
+    FirstPersonController playerControl;
+    camMouseLook cameraControl;
+
+    GameObject inventory;
+
+    public bool inventoryOpen;
+    public bool canOpen;
+
+    void Start () {
+        playerControl = GetComponent<FirstPersonController>();
+        cameraControl = GetComponentInChildren<camMouseLook>();
+
+        inventory = GameObject.FindGameObjectWithTag("Inventory");
+
+        inventory.SetActive(false);
+        inventoryOpen = false;
+	}
+	
+
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Tab) && canOpen)
+        {
+            playerControl.enabled = false;
+            cameraControl.enabled = false;
+            inventory.SetActive(true);
+            cameraControl.transform.LookAt(inventory.transform.position);
+            inventoryOpen = true;
+
+        }
+        if (Input.GetKeyDown(KeyCode.Tab) && !canOpen)
+        {
+            playerControl.enabled = true ;
+            cameraControl.enabled = true ;
+            inventory.SetActive(false);
+            inventoryOpen = false;
+        }
+
+        if (inventoryOpen)
+        {
+            canOpen = false;
+        }
+        else
+        {
+            canOpen = true;
+        }
+    }
+}
