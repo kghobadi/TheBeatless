f8b303d17f99795f032084c39e4929497577e7fb
Unknown <ksg304@nyu.edu>
2017-10-08 20:45:16 -0400

    Added some lil sounds for inventory

--- Assets/Inventory.cs
+++ Assets/Inventory.cs
@@ -12,11 +12,16 @@ public class Inventory : MonoBehaviour {
     public bool inventoryOpen;
     public bool canOpen;
 
+    AudioSource soundBoard;
+    public AudioClip bagOpen;
+    public AudioClip bagClose;
+
     void Start () {
         playerControl = GetComponent<FirstPersonController>();
         cameraControl = GetComponentInChildren<camMouseLook>();
 
         inventory = GameObject.FindGameObjectWithTag("Inventory");
+        soundBoard = GetComponentInChildren<AudioSource>();
 
         inventory.SetActive(false);
         inventoryOpen = false;
@@ -31,6 +36,7 @@ public class Inventory : MonoBehaviour {
             inventory.SetActive(true);
             cameraControl.transform.LookAt(inventory.transform.position);
             inventoryOpen = true;
+            soundBoard.PlayOneShot(bagOpen);
 
         }
         if (Input.GetKeyDown(KeyCode.Tab) && !canOpen)
@@ -39,6 +45,7 @@ public class Inventory : MonoBehaviour {
             cameraControl.enabled = true ;
             inventory.SetActive(false);
             inventoryOpen = false;
+            soundBoard.PlayOneShot(bagClose);
         }
 
         if (inventoryOpen)
