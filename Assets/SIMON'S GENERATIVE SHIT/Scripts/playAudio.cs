8ca2db682324cf38602f922f46018a68f5cc4900
yustygamedev <sp3990@nyu.edu>
2017-10-19 14:10:32 -0400

    yoooo we generatin' sounds by zounds

--- Assets/SIMON'S GENERATIVE SHIT/Scripts/playAudio.cs
+++ Assets/SIMON'S GENERATIVE SHIT/Scripts/playAudio.cs
@@ -8,15 +8,62 @@ public class playAudio : MonoBehaviour {
 	public AudioClip[] clips;
 	public bool playedAudio = false;
 	AudioSource audio;
+	public int timeScale = 0;
+
+	private	void Awake() {
+
+		SimpleClock.ThirtySecond += OnThirtySecond;
+		//audio.clip = clips [Random.Range (0, 1)];
+	}
+
+	private void OnDestroy(){
+		SimpleClock.ThirtySecond -= OnThirtySecond;
+	}
+
+
+
+	void OnThirtySecond(BeatArgs e)
+	{
+		if (e.TickMask [TickValue.Eighth]) {
+		//	audio.clip = clips [Random.Range (0, 1)];
+			//audio.Play ();
+		}
+
+
+	}
 
 	// Use this for initialization
 	void Start () {
 		audio = GetComponent<AudioSource>();
+		audio.clip = clips [Random.Range (0, 4)];
+		timeScale = Random.Range (0, 4);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-			if (GetComponent<growPlants> ().growthNumber == 1) {
+		if (GetComponent<growPlants> ().growthNumber == 1) {
+			if (!audio.isPlaying) {
+				//timeScale = Random.Range (0, 4);
+				
+				if (timeScale == 1) {
+					audio.PlayScheduled (SimpleClock.AtNextEighth ());
+					audio.SetScheduledEndTime (SimpleClock.AtNextQuarter ());
+				} else if (timeScale == 2) {
+					audio.PlayScheduled (SimpleClock.AtNextHalf ());
+					audio.SetScheduledEndTime (SimpleClock.AtNextMeasure () + SimpleClock.AtNextHalf());
+				} else if (timeScale == 3) {
+					audio.PlayScheduled (SimpleClock.AtNextQuarter ());
+					audio.SetScheduledEndTime (SimpleClock.AtNextQuarter () + SimpleClock.AtNextHalf());
+				} else if (timeScale == 0) {
+					audio.PlayScheduled (SimpleClock.AtNextMeasure ());
+					audio.SetScheduledEndTime (SimpleClock.AtNextMeasure () + SimpleClock.AtNextMeasure());
+				}
+			}
+		} else {
+			audio.Stop ();
+		}
+		//audio.PlayScheduled (SimpleClock.AtNextEighth());
+		/*	if (GetComponent<growPlants> ().growthNumber == 1) {
 			if (!playedAudio) {
 				audio.clip = clips [Random.Range (0, 1)];
 				playedAudio = true;
@@ -46,5 +93,6 @@ public class playAudio : MonoBehaviour {
 		if (!audio.isPlaying) {
 			audio.Play ();
 		}
+		*/
 	}
 }
--- Assets/diffVisFeedback.cs
+++ Assets/diffVisFeedback.cs
@@ -29,14 +29,14 @@ public class diffVisFeedback : MonoBehaviour {
         playSounds2 = gameObject.AddComponent<AudioSource>();
         playSounds3 = gameObject.AddComponent<AudioSource>();
         playSounds4 = gameObject.AddComponent<AudioSource>();
-<<<<<<< HEAD
+//<<<<<<< HEAD
 
-=======
+//=======
 		playSounds.spatialBlend = 1;
 		playSounds2.spatialBlend = 1;
 		playSounds3.spatialBlend = 1;
 		playSounds4.spatialBlend = 1;
->>>>>>> master
+//>>>>>>> master
 
         playSounds.playOnAwake = false;
         playSounds2.playOnAwake = false;
