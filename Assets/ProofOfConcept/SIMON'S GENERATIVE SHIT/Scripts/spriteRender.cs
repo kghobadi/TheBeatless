90458c697ee37995e94ea88036960d9578a47325
Unknown <ksg304@nyu.edu>
2017-10-24 14:16:34 -0400

    I moved it! Terrain stuff and NO MORE OLD SHIT BIH

---
+++ Assets/ProofOfConcept/SIMON'S GENERATIVE SHIT/Scripts/spriteRender.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spriteRender : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GetComponentInParent<playAudio> ().sprVisisble == false) {
+			GetComponent<SpriteRenderer> ().enabled = false;
+		} else {
+			GetComponent<SpriteRenderer> ().enabled = true;
+		}
+	}
+}
