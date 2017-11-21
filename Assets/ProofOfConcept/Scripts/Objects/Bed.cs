b9a5c480ed3d7739265fe18e93309f52b3dfc5fb
Easton <eastonself@gmail.com>
2017-11-21 13:31:47 -0500

    made more changes too?

--- Assets/ProofOfConcept/Scripts/Objects/Bed.cs
+++ Assets/ProofOfConcept/Scripts/Objects/Bed.cs
@@ -22,6 +22,7 @@ public class Bed : Interactable
 
 	public int windDir = 0;
 	ParticleSystem wond;
+	ParticleSystem windBlast;
     Sun sunScript;
 
     Vector3 sunStartPos;
@@ -33,7 +34,7 @@ public class Bed : Interactable
 
     public int dayCounter;
 
-    float speedMultiplier;
+    public float speedMultiplier;
 
     public override void Start()
 	{
@@ -48,7 +49,7 @@ public class Bed : Interactable
         sunScript = sun.GetComponent<Sun>();
         sunStartPos = sun.transform.position;
 		wond = GameObject.Find ("windParticles").GetComponent<ParticleSystem> ();
-
+		windBlast = GameObject.Find ("windBlast").GetComponent<ParticleSystem> ();
         originalPSpeed = fpc.speed;
     }
 
@@ -104,6 +105,7 @@ public class Bed : Interactable
             fpc.isAwake = false;
 			setDayPassed = false;
             dayCounter++;
+
             //clock.bpm = Random.Range(minBpm, maxBpm);
             speedMultiplier = clock.bpm / minBpm;
             ParticleSystem.MainModule wondModule = wond.main;
@@ -112,7 +114,9 @@ public class Bed : Interactable
             windDir = Random.Range(0, 4);
             switch (windDir) {
 			case 0: //left to right
-				wond.transform.eulerAngles = new Vector3(-90, 0, -90);
+				wond.transform.eulerAngles = new Vector3 (-90, 0, -90);
+				windBlast.transform.eulerAngles = wond.transform.eulerAngles;
+
 				//set the start of particles
 				//set their motion
 				//set simulation speed here when you've fucked w the BPM thingy
@@ -120,14 +124,17 @@ public class Bed : Interactable
 
 			case 1: //bottom to top
 				wond.transform.eulerAngles = new Vector3(-90, 0, 180);
+				windBlast.transform.eulerAngles = wond.transform.eulerAngles;
 			break;
 
 			case 2: //top to bottom
 				wond.transform.eulerAngles = new Vector3(-90, 0, 0);
+				windBlast.transform.eulerAngles = wond.transform.eulerAngles;
 			break;
 
 			case 3: //right to left
 				wond.transform.eulerAngles = new Vector3(-90, 0, 90);
+				windBlast.transform.eulerAngles = wond.transform.eulerAngles;
 			break;
 
 
